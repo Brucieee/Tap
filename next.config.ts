037bfe3a0958674771c3a7499c7d225bc4638b39
Brucieee <brucewaynelim9@gmail.com>
2026-05-26 13:51:07 +0800

    chore: move outputFileTracingIncludes to top-level property for next 16

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['playwright', 'playwright-core']
+  serverExternalPackages: ['playwright', 'playwright-core'],
+  outputFileTracingIncludes: {
+    '/api/**/*': ['node_modules/playwright-core/browsers.json'],
+  },
 };
 
 export default nextConfig;
